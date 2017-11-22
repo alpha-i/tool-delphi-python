58e4a5180392a8bed336d0e2f6b5a3d494dd0061
Gabriele Alese <gabriele.alese@alpha-i.co>
2017-11-22 15:33:21 +0000

    Scheduler supports iteration protocol

--- delphi/scheduler/abstract_scheduler.py
+++ delphi/scheduler/abstract_scheduler.py
@@ -17,16 +17,16 @@ class SchedulingFrequencyType(Enum):
 
 
 class SchedulingFrequency:
-    def __init__(self, schedule_frequency, days_offset=0, minutes_offset=0):
+    def __init__(self, frequency_type, days_offset=0, minutes_offset=0):
         """
-        :param schedule_frequency: Type of the scheduling frequency
-        :type schedule_frequency: SchedulingFrequencyType
+        :param frequency_type: Type of the scheduling frequency
+        :type frequency_type: SchedulingFrequencyType
         :param days_offset:
         :type days_offset: int
         :param minutes_offset:
         :type minutes_offset: int
         """
-        self.frequency = schedule_frequency
+        self.frequency_type = frequency_type
         self.minutes_offset = minutes_offset
         self.days_offset = days_offset
 
@@ -85,6 +85,10 @@ class Scheduler(AbstractScheduler):
         self._init_a_schedule(self.training_scheduling, OracleAction.TRAIN)
         self._init_a_schedule(self.prediction_scheduling, OracleAction.PREDICT)
 
+    def __iter__(self):
+        for schedule_day in sorted(self.schedule.keys()):
+            yield schedule_day, list(self.schedule[schedule_day])
+
     def _get_scheduled_days(self, calendar, offset, start_date, end_date):
         week_days = filter(lambda x: x.weekday() == offset,
                            rrule.rrule(rrule.DAILY, dtstart=start_date, until=end_date))
@@ -98,9 +102,16 @@ class Scheduler(AbstractScheduler):
         return result
 
     def _init_a_schedule(self, schedule, action):
+        """
+        :param schedule:
+        :type schedule: SchedulingFrequency
+        :param action:
+        :type action: OracleAction
+        :return:
+        """
         exchange_calendar = calendar.get_calendar(self.exchange_name)
         exchange_schedule = exchange_calendar.schedule(self.start_date, self.end_date)
-        if schedule.type == SchedulingFrequencyType.WEEKLY:
+        if schedule.frequency_type == SchedulingFrequencyType.WEEKLY:
             # get a list of all the days where the action should take place
             # (.weekday is the same as the offset: 0 is a Monday)
             training_days = self._get_scheduled_days(exchange_calendar, schedule.days_offset, self.start_date,
@@ -111,14 +122,14 @@ class Scheduler(AbstractScheduler):
                 scheduled_time = market_open + datetime.timedelta(minutes=schedule.minutes_offset)
                 self.schedule[scheduled_time].add(action)
 
-        elif schedule.type == SchedulingFrequencyType.DAILY:
+        elif schedule.frequency_type == SchedulingFrequencyType.DAILY:
             market_days = exchange_calendar.valid_days(self.start_date, self.end_date)
             for day in market_days:
                 market_open = exchange_schedule.loc[day, "market_open"]
                 scheduled_time = market_open + datetime.timedelta(minutes=schedule.minutes_offset)
                 self.schedule[scheduled_time].add(action)
 
-        elif schedule.type == SchedulingFrequencyType.MINUTE:
+        elif schedule.frequency_type == SchedulingFrequencyType.MINUTE:
             for day in exchange_calendar.valid_days(self.start_date, self.end_date):
                 market_open = exchange_schedule.loc[day, "market_open"]
                 market_close = exchange_schedule.loc[day, "market_close"]
--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -14,10 +14,10 @@ def test_weekly_prediction_and_training_scheduler():
         exchange_name='NYSE',
         prediction_horizon=datetime.timedelta(days=1),
         prediction_scheduling=SchedulingFrequency(
-            schedule_frequency=SchedulingFrequencyType.WEEKLY, days_offset=0, minutes_offset=30
+            frequency_type=SchedulingFrequencyType.WEEKLY, days_offset=0, minutes_offset=30
         ),  # every Monday, 30m after market start
         training_scheduling=SchedulingFrequency(
-            schedule_frequency=SchedulingFrequencyType.WEEKLY, days_offset=0, minutes_offset=30
+            frequency_type=SchedulingFrequencyType.WEEKLY, days_offset=0, minutes_offset=30
         ),  # every Monday, 30m after market start
     )
     events = test_scheduler.get_event(datetime.datetime(2017, 11, 6, 15, 0, tzinfo=pytz.UTC))
@@ -34,10 +34,10 @@ def test_weekly_prediction_scheduler_starting_on_tuesday():
         exchange_name='NYSE',
         prediction_horizon=datetime.timedelta(days=1),
         prediction_scheduling=SchedulingFrequency(
-            schedule_frequency=SchedulingFrequencyType.WEEKLY, days_offset=1, minutes_offset=30
+            frequency_type=SchedulingFrequencyType.WEEKLY, days_offset=1, minutes_offset=30
         ),  # every Tuesday, 30m after market start
         training_scheduling=SchedulingFrequency(
-            schedule_frequency=SchedulingFrequencyType.WEEKLY, days_offset=1, minutes_offset=30
+            frequency_type=SchedulingFrequencyType.WEEKLY, days_offset=1, minutes_offset=30
         ),  # every Tuesday, 30m after market start
     )
 
@@ -58,10 +58,10 @@ def test_weekly_prediction_scheduler_with_a_public_holiday():
         exchange_name='NYSE',
         prediction_horizon=datetime.timedelta(days=1),
         prediction_scheduling=SchedulingFrequency(
-            schedule_frequency=SchedulingFrequencyType.WEEKLY, days_offset=0, minutes_offset=15
+            frequency_type=SchedulingFrequencyType.WEEKLY, days_offset=0, minutes_offset=15
         ),  # every Monday, 15m after market start
         training_scheduling=SchedulingFrequency(
-            schedule_frequency=SchedulingFrequencyType.WEEKLY, days_offset=0, minutes_offset=15
+            frequency_type=SchedulingFrequencyType.WEEKLY, days_offset=0, minutes_offset=15
         ),  # every Monday, 15m after market start
     )
 
@@ -85,10 +85,10 @@ def test_daily_prediction_scheduler():
         exchange_name='NYSE',
         prediction_horizon=datetime.timedelta(days=1),
         prediction_scheduling=SchedulingFrequency(
-            schedule_frequency=SchedulingFrequencyType.DAILY, minutes_offset=15
+            frequency_type=SchedulingFrequencyType.DAILY, minutes_offset=15
         ),  # every day, 15m after market start
         training_scheduling=SchedulingFrequency(
-            schedule_frequency=SchedulingFrequencyType.DAILY,
+            frequency_type=SchedulingFrequencyType.DAILY,
             minutes_offset=15
         ),  # every day, 30m after market start
     )
@@ -111,9 +111,28 @@ def test_minute_prediction_scheduler():
         end_date=datetime.datetime(2017, 12, 20, 18, 0),
         exchange_name='NYSE',
         prediction_horizon=datetime.timedelta(days=1),
-        prediction_scheduling=SchedulingFrequency(schedule_frequency=SchedulingFrequencyType.MINUTE),
-        training_scheduling=SchedulingFrequency(schedule_frequency=SchedulingFrequencyType.MINUTE)
+        prediction_scheduling=SchedulingFrequency(frequency_type=SchedulingFrequencyType.MINUTE),
+        training_scheduling=SchedulingFrequency(frequency_type=SchedulingFrequencyType.MINUTE)
     )
 
     assert sorted(list(test_scheduler.schedule.keys()))[0] == datetime.datetime(2017, 12, 20, 14, 30, tzinfo=pytz.UTC)
     assert sorted(list(test_scheduler.schedule.keys()))[-1] == datetime.datetime(2017, 12, 20, 21, 0, tzinfo=pytz.UTC)
+
+
+def test_scheduler_works_as_iterator():
+    test_scheduler = Scheduler(
+        start_date=datetime.datetime(2017, 11, 6),
+        end_date=datetime.datetime(2017, 11, 21),
+        exchange_name='NYSE',
+        prediction_horizon=datetime.timedelta(days=1),
+        prediction_scheduling=SchedulingFrequency(
+            frequency_type=SchedulingFrequencyType.WEEKLY, days_offset=1, minutes_offset=30
+        ),  # every Tuesday, 30m after market start
+        training_scheduling=SchedulingFrequency(
+            frequency_type=SchedulingFrequencyType.WEEKLY, days_offset=1, minutes_offset=30
+        ),  # every Tuesday, 30m after market start
+    )
+
+    for day, events in test_scheduler:
+        assert isinstance(day, datetime.datetime)
+        assert isinstance(events, list)
