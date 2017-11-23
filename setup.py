566c222f91d2c597b7c9b1d317b6486024121eac
Gabriele Alese <gabriele.alese@alpha-i.co>
2017-11-23 10:01:21 +0000

    Fix setup.py to install
    Correct package name and dependencies.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
 
-setup(name='alphai_prototype_env',
-      description='Alpha-I Prototype Environment',
-      author='Richard Mason',
-      author_email='richard.mason@alpha-i.co',
-      version='0.0.1',
-      packages=find_packages(exclude=['doc', 'tests*']),
-      install_requires=[
-            'numpy>=1.11.3'
-      ]
+setup(
+    name='delphi',
+    description='Delphi: Alpha-I Prototype Environment',
+    author='Gabriele Alese, Richard Mason',
+    author_email='gabriele.alese@alpha-i.co, richard.mason@alpha-i.co',
+    version='0.1.0',
+    packages=find_packages(exclude=['doc', 'tests*']),
+    install_requires=[
+        'numpy>=1.11.3',
+        'pandas==0.19.2',
+        'pandas-market-calendars==0.11',
+        'alphai_finance==1.3.1',
+        'dateutils==0.6.6'
+    ]
 )
