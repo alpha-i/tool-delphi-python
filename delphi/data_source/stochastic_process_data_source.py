6047892e4ac1f5a15af4d346e8f9023bd412cf2b
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2017-11-29 15:36:21 +0000

    removed an uncessary creation of ohlcv dict

--- delphi/data_source/stochastic_process_data_source.py
+++ delphi/data_source/stochastic_process_data_source.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 import pytz
-from datetime import timedelta, datetime
-from copy import deepcopy
 import pandas_market_calendars as mcal
 from delphi.data_source.abstract_data_source import AbstractDataSource
 from alphai_time_series.make_series import random_walks
@@ -43,7 +41,6 @@ class StochasticProcessDataSource(AbstractDataSource):
 
         return values_for_symbols.loc[current_datetime]
 
-
     @staticmethod
     def _create_minute_datetime_index(exchange_name, start_date, end_date):
         calendar = mcal.get_calendar(exchange_name)
@@ -58,34 +55,10 @@ class StochasticProcessDataSource(AbstractDataSource):
 
         return datetime_index
 
-    @staticmethod
-    def _make_ohlcv_dict(dataframe):
-        """
-
-        :param dataframe: pandas dataframe
-        :return: dict that with keys open high low close volume and the given dataframe as value
-        """
-        open = deepcopy(dataframe)  # FIXME change the var open -> open_vals
-
-        high = deepcopy(dataframe)
-        high = 1.1 * high
-
-        low = deepcopy(dataframe)
-        low = 0.9 * low
-
-        close = deepcopy(dataframe)
-        close.values[:-1, :] = open.values[1:, :]
-
-        volume = deepcopy(dataframe)
-        volume = volume.round(0).astype(int) + 1
-
-        ohlcv_dict = {'open': open, 'high': high, 'low': low, 'close': close, 'volume': volume}
-
-        return ohlcv_dict
-
     def _setup_data(self):
         exchange_name = self.config['exchange']
-        time_index = self._create_minute_datetime_index(exchange_name=exchange_name, start_date=self.start(),
+        time_index = self._create_minute_datetime_index(exchange_name=exchange_name,
+                                                        start_date=self.start(),
                                                         end_date=self.end())
         correlation_coeff = 0.1
         offset = correlation_coeff / 5.
@@ -102,5 +75,5 @@ class StochasticProcessDataSource(AbstractDataSource):
         columns = ["walk_{}".format(clm) for clm in range(n_series)]
         stochastic_process_prices = pd.DataFrame(data=stochastic_process_output, index=time_index, columns=columns)
 
-        self._data_dict = self._make_ohlcv_dict(stochastic_process_prices)
+        self._data_dict["close"] = stochastic_process_prices
 
