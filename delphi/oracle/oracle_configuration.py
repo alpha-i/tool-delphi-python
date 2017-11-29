b0c63b7285ca2b35e2c40713766febd2b93e4ed3
Daniele Murroni <daniele.murroni@gmail.com>
2017-11-29 11:28:17 +0000

    Express prediction and training delta in days. Added some documentations. Solves issue #16

--- delphi/oracle/oracle_configuration.py
+++ delphi/oracle/oracle_configuration.py
@@ -7,18 +7,28 @@ class OracleConfiguration:
 
     def __init__(self, config):
         """
-        Loads a dict containing the configuration
+        Loads a dict containing the configuration: Example.
         {
-        'scheduling': {
-            'prediction_horizon': '',
-            'prediction_offset': '',
-            'prediction_frequency': '',
-            'prediction_delta': '',
-
-            'training_frequency': '',
-            'training_delta': '',
-        },
-        'oracle': {
+            'scheduling': {
+                'prediction_horizon': 240, # how many HOURS in the future you want to predict
+                'prediction_frequency':
+                    {
+                        'frequency_type': 'DAILY', # frequency type
+                        'days_offset': 0, # days offset from the beginning of the week
+                        'minutes_offset': 15 # minutes offset from the market opening
+                    },
+                'prediction_delta': 10, # how many DAYS of data are needed for the prediction
+            
+                'training_frequency':
+                    {
+                        'frequency_type': 'WEEKLY', # frequency type
+                        'days_offset': 0, # days offset from the beginning of the week
+                        'minutes_offset': 15 # minutes offset from the market opening
+                    },
+                'training_delta': 20, # how many DAYS of data are needed for the training
+            },
+            'oracle': {
+                # oracle dependent parameters
             }
         }
         :param config:
