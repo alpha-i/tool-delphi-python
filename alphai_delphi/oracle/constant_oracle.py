efa260a0e652a7248f7cf8d58e6b31a6d4d315ce
Gabriele Alese <gabriele.alese@alpha-i.co>
2018-01-18 11:30:20 +0000

    Nuke comments

--- alphai_delphi/oracle/constant_oracle.py
+++ alphai_delphi/oracle/constant_oracle.py
@@ -37,23 +37,6 @@ class ConstantOracle(AbstractOracle):
         pass
 
     def predict(self, data, current_timestamp, target_timestamp):
-
-        # constant_variance = self.config["constant_variance"]
-        #
-        # close_df = data.sel(raw_features='close').to_dataframe().drop(labels=["raw_features"], axis=1)
-        #
-        # symbols = close_df.columns
-        # num_symbols = len(symbols)
-        #
-        # mean = close_df.iloc[-1]
-        # mean.name = mean.name.tz_localize('America/New_York').astimezone('UTC')
-        #
-        # covariance = pd.DataFrame(data=constant_variance * np.eye(num_symbols), index=symbols, columns=symbols)
-        #
-        # prediction = PredictionResult(mean, covariance, current_timestamp, target_timestamp)
-        #
-        # return prediction
-
         constant_variance = self.config["constant_variance"]
 
         symbols = data['close'].columns
