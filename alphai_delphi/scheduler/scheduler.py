93700679366d943c2af9f451b339b318283fadc4
Daniele Murroni <daniele.murroni@gmail.com>
2018-03-16 16:55:00 +0000

    Fixed Monthly scheduling Generation (#71)

--- alphai_delphi/scheduler/scheduler.py
+++ alphai_delphi/scheduler/scheduler.py
@@ -156,7 +156,8 @@ class Scheduler(AbstractScheduler):
         is_offset_positive = day_offset_of_the_month >= 0
 
         monthly_schedule = []
-        for first_day_of_the_month in rrule.rrule(rrule.MONTHLY, dtstart=self.start_date, until=self.end_date):
+        start_date = self.start_date.replace(day=1)
+        for first_day_of_the_month in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=self.end_date):
             if is_offset_positive:
                 new_day = first_day_of_the_month + datetime.timedelta(days=day_offset_of_the_month)
             else:
@@ -165,19 +166,21 @@ class Scheduler(AbstractScheduler):
 
             monthly_schedule.append(new_day)
 
-        end_date = self.end_date if is_offset_positive else self.end_date + datetime.timedelta(days=31)
+        if is_offset_positive:
+            end_date = self.end_date
+        else:
+            first, last = calendar.monthrange(self.end_date.year, self.end_date.month)
+            end_date = self.end_date.replace(day=last)
+
         schedule = self.calendar.schedule(self.start_date, end_date)
         valid_days = self.calendar.valid_days(self.start_date, end_date)
-        scheduled_days = []
         day_offset = 1 if is_offset_positive else -1
 
+        computed_moments = []
         for day in monthly_schedule:
             while day not in valid_days:
                 day += datetime.timedelta(days=day_offset)
-            scheduled_days.append(day)
 
-        computed_moments = []
-        for day in scheduled_days:
             market_open = schedule.loc[day, "market_open"]
             scheduled_time = market_open + datetime.timedelta(minutes=scheduling_frequency['minutes_offset'])
             computed_moments.append(scheduled_time)
--- tests/scheduler/test_scheduler.py
+++ tests/scheduler/test_scheduler.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from pandas import Timestamp
 
 from alphai_delphi.scheduler.scheduler import Scheduler
@@ -205,3 +206,66 @@ def test_scheduler_monthly():
         Timestamp('2018-01-31 14:30:00+0000', tz='UTC'),
         Timestamp('2018-02-28 14:30:00+0000', tz='UTC'),
     ]
+
+
+def test_scheduler_end_of_month():
+
+    test_scheduler = Scheduler(
+        start_date=datetime.datetime(2006, 11, 15, tzinfo=pytz.utc),
+        end_date=datetime.datetime(2017, 9, 12, tzinfo=pytz.utc),
+        calendar_name='JSE',
+        prediction_frequency=dict(frequency_type=SchedulingFrequencyType.MONTHLY, days_offset=-1),
+        training_frequency=dict(frequency_type=SchedulingFrequencyType.MONTHLY, days_offset=-1),
+    )
+
+    end_of_month_days = ['2017-09-29', '2017-08-31', '2017-07-31', '2017-06-30', '2017-05-31', '2017-04-28',
+                         '2017-03-31', '2017-02-28', '2017-01-31', '2016-12-30', '2016-11-30', '2016-10-31',
+                         '2016-09-30', '2016-08-31', '2016-07-29', '2016-06-30', '2016-05-31', '2016-04-29',
+                         '2016-03-31', '2016-02-29', '2016-01-29', '2015-12-31', '2015-11-30', '2015-10-30',
+                         '2015-09-30', '2015-08-31', '2015-07-31', '2015-06-30', '2015-05-29', '2015-04-30',
+                         '2015-03-31', '2015-02-27', '2015-01-30', '2014-12-31', '2014-11-28', '2014-10-31',
+                         '2014-09-30', '2014-08-29', '2014-07-31', '2014-06-30', '2014-05-30', '2014-04-30',
+                         '2014-03-31', '2014-02-28', '2014-01-31', '2013-12-31', '2013-11-29', '2013-10-31',
+                         '2013-09-30', '2013-08-30', '2013-07-31', '2013-06-28', '2013-05-31', '2013-04-30',
+                         '2013-03-28', '2013-02-28', '2013-01-31', '2012-12-31', '2012-11-30', '2012-10-31',
+                         '2012-09-28', '2012-08-31', '2012-07-31', '2012-06-29', '2012-05-31', '2012-04-30',
+                         '2012-03-30', '2012-02-29', '2012-01-31', '2011-12-30', '2011-11-30', '2011-10-31',
+                         '2011-09-30', '2011-08-31', '2011-07-29', '2011-06-30', '2011-05-31', '2011-04-29',
+                         '2011-03-31', '2011-02-28', '2011-01-31', '2010-12-31', '2010-11-30', '2010-10-29',
+                         '2010-09-30', '2010-08-31', '2010-07-30', '2010-06-30', '2010-05-31', '2010-04-30',
+                         '2010-03-31', '2010-02-26', '2010-01-29', '2009-12-31', '2009-11-30', '2009-10-30',
+                         '2009-09-30', '2009-08-31', '2009-07-31', '2009-06-30', '2009-05-29', '2009-04-30',
+                         '2009-03-31', '2009-02-27', '2009-01-30', '2008-12-31', '2008-11-28', '2008-10-31',
+                         '2008-09-30', '2008-08-29', '2008-07-31', '2008-06-30', '2008-05-30', '2008-04-30',
+                         '2008-03-31', '2008-02-29', '2008-01-31', '2007-12-31', '2007-11-30', '2007-10-31',
+                         '2007-09-28', '2007-08-31', '2007-07-31', '2007-06-29', '2007-05-31', '2007-04-30',
+                         '2007-03-30', '2007-02-28', '2007-01-31', '2006-12-29', '2006-11-30']
+
+    def as_timestamp(x):
+        return pd.Timestamp(x, tz='UTC').replace(hour=7)
+
+    expected_timestamp_list = sorted(map(as_timestamp, end_of_month_days))
+    returned_timestamp_list = sorted(list(test_scheduler.schedule.keys()))
+    assert len(returned_timestamp_list) == len(expected_timestamp_list)
+    assert returned_timestamp_list == expected_timestamp_list
+
+
+def test_february():
+
+    test_scheduler = Scheduler(
+        start_date=datetime.datetime(2007, 1, 30, tzinfo=pytz.utc),
+        end_date=datetime.datetime(2007, 4, 1, tzinfo=pytz.utc),
+        calendar_name='JSE',
+        prediction_frequency=dict(frequency_type=SchedulingFrequencyType.MONTHLY, days_offset=-1),
+        training_frequency=dict(frequency_type=SchedulingFrequencyType.MONTHLY, days_offset=-1),
+    )
+
+    end_of_month_days = ['2007-04-30', '2007-03-30', '2007-02-28', '2007-01-31']
+
+    def as_timestamp(x):
+        return pd.Timestamp(x, tz='UTC').replace(hour=7)
+
+    expected_timestamp_list = sorted(map(as_timestamp, end_of_month_days))
+    returned_timestamp_list = sorted(list(test_scheduler.schedule.keys()))
+    assert len(returned_timestamp_list) == len(expected_timestamp_list)
+    assert returned_timestamp_list == expected_timestamp_list
