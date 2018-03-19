ae8985a124336530bac8607d1d9e7d9abbc1614a
Daniele Murroni <daniele.murroni@gmail.com>
2018-03-19 17:58:17 +0000

    Reame function

--- alphai_delphi/controller/__init__.py
+++ alphai_delphi/controller/__init__.py
@@ -24,7 +24,7 @@ class Controller(AbstractController):
         for prediction_moment, events in self.scheduler:
             for action in events:
                 oracle_interval = self.oracle.get_delta_for_event(action)
-                interval = self.get_market_interval(prediction_moment, oracle_interval)
+                interval = self.calculate_interval(prediction_moment, oracle_interval)
                 raw_data = self.datasource.get_data(prediction_moment, interval)
 
                 if action == OracleAction.TRAIN:
@@ -50,7 +50,7 @@ class Controller(AbstractController):
         self.performance.create_oracle_report()
         self.print_run_summary()
 
-    def get_market_interval(self, moment, oracle_interval):
+    def calculate_interval(self, moment, oracle_interval):
         """
         Given a moment and an interval in DAYS from the oracle,it calculates a real delta in order to get
         a delta that gives you the valid number of timesteps in the data
