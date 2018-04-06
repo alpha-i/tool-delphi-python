ac2f0ce3c18f88fd448608e7d42adb26c7618b4c
Daniele Murroni <daniele.murroni@gmail.com>
2018-04-06 10:23:17 +0100

    release dev version of 3.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Delphi: Alpha-I Prototype Environment',
     author='Gabriele Alese, Richard Mason, Daniele Murroni',
     author_email='gabriele.alese@alpha-i.co, richard.mason@alpha-i.co, daniele.murroni@alpha-i.co',
-    version='3.0.0',
+    version='3.0.0-dev',
     packages=find_packages(exclude=['doc', 'tests*']),
     install_requires=[
         "numpy",
